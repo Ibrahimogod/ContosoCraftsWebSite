73a66b2dc9e131396c3d456ed8d595c663dae256
Ibrahim Ahmad Badr <ibadr2122000@gmail.com>
2021-05-14 19:14:53 +0200

    ASP.NET Core WepApp

---
+++ ContosoCrafts.WebSite/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoCrafts.WebSite.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public JsonFileProductService ProductService { get; }
+
+        public ProductsController(JsonFileProductService productService)
+        {
+            this.ProductService = productService;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> Get() => ProductService.GetProducts();
+
+        [Route("Rate")]
+        [HttpGet]
+        public ActionResult Get([FromQuery] string Id,[FromQuery] int rating)
+        {
+            return Ok();
+        }
+
+
+        
+
+    }
+}
